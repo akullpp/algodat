19e2e7daa6832dbd33971d88eabbbd2bc1b30eca
Andreas Kull <akullpp@gmail.com>
2017-10-17 16:47:22 +0200

    Refactor Binary Expression Tree tests

--- src/test/java/de/akull/datastructures/binaryexpressiontree/BinaryExpressionTreeTest.java
+++ src/test/java/de/akull/datastructures/binaryexpressiontree/BinaryExpressionTreeTest.java
@@ -1,49 +1,101 @@
 package de.akull.datastructures.binaryexpressiontree;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(JUnit4.class)
 public class BinaryExpressionTreeTest {
-
-    private Node conjunction() {
-        return new And(new Equal("a", "1"), new Equal("b", "2"));
-    }
 
     @Test
     public void Should_Evaluate_Conjunctions() {
+        Node conjunction = new And(new Equal("a", "1"), new Equal("b", "2"));
         HashMap<String, String> data = new HashMap<>();
         data.put("a", "1");
         data.put("b", "2");
 
-        boolean result = conjunction().eval(data);
+        boolean result = conjunction.eval(data);
 
         assertThat(result).isTrue();
     }
 
     @Test
-    public void Should_Evaluate_Incomplete_Conjunctions_As_False() {
-        Node tree = new And(new Equal("a", "1"), new Equal("b", "2"));
+    public void Should_Evaluate_Invalid_Conjunctions_As_False() {
+        Node conjunction = new And(new Equal("a", "1"), new Equal("b", "2"));
         HashMap<String, String> data = new HashMap<>();
-        data.put("a", "1");
+        data.put("a", "2");
+        data.put("a", "2");
 
-        boolean result = conjunction().eval(data);
+        boolean result = conjunction.eval(data);
 
         assertThat(result).isFalse();
     }
 
     @Test
-    public void Should_Evaluate_Non_Matching_Conjunctions_As_False() {
+    public void Should_Evaluate_Disjunctions() {
+        Node disjunction = new Or(new Equal("a", "1"), new Equal("b", "2"));
         HashMap<String, String> data = new HashMap<>();
         data.put("a", "1");
         data.put("b", "3");
 
-        boolean result = conjunction().eval(data);
+        boolean result = disjunction.eval(data);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void Should_Evaluate_Invalid_Disjunctions_As_False() {
+        Node disjunction = new Or(new Equal("a", "1"), new Equal("b", "2"));
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "2");
+        data.put("b", "1");
+
+        boolean result = disjunction.eval(data);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void Should_Evaluate_Equal() {
+        Node equal = new Equal("a", "1");
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "1");
+
+        boolean result = equal.eval(data);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void Should_Evaluate_Non_Matching_Equal_As_False() {
+        Node equal = new Equal("a", "1");
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "2");
+
+        boolean result = equal.eval(data);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void Should_Evaluate_Not() {
+        Node equal = new Not(new Equal("a", "1"));
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "2");
+
+        boolean result = equal.eval(data);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void Should_Evaluate_Non_Matching_Not_As_False() {
+        Node equal = new Not(new Equal("a", "1"));
+        HashMap<String, String> data = new HashMap<>();
+        data.put("a", "1");
+
+        boolean result = equal.eval(data);
 
         assertThat(result).isFalse();
     }
