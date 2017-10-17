fa7ca50b7e31ef872565b5825b97551e2fd62946
Andreas Kull <akullpp@gmail.com>
2017-10-17 13:12:23 +0200

    Add Binary Search

---
+++ src/main/java/de/akull/algorithms/BinarySearch.java
@@ -0,0 +1,24 @@
+package de.akull.algorithms;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static int search(int[] xs, int x) {
+        int pivot = xs.length / 2;
+
+        if (x == xs[pivot]) {
+            return 1;
+        }
+        if (xs.length == 1) {
+            return -1;
+        }
+        if (x < xs[pivot]) {
+            return search(Arrays.copyOfRange(xs, 0, pivot - 1), x);
+        }
+        if (x > xs[pivot]) {
+            return search(Arrays.copyOfRange(xs, pivot, xs.length), x);
+        }
+        return -1;
+    }
+}
