94756c33f7e96ef4867d2602902916f751774abf
akullpp <akullpp@gmail.com>
2017-10-17 13:12:23 +0200

    Add Binary Search

---
+++ src/test/java/de/akull/algorithms/BinarySearchTest.java
@@ -0,0 +1,28 @@
+package de.akull.algorithms;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BinarySearchTest {
+
+    private final int[] SEARCH_SPACE = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    @Test
+    public void Should_Find_Element_If_Present() {
+        int search = 7;
+
+        int result = BinarySearch.search(SEARCH_SPACE, search);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void Should_Not_Find_Element_If_Not_Present() {
+        int search = 10;
+
+        int result = BinarySearch.search(SEARCH_SPACE, search);
+
+        assertThat(result).isEqualTo(-1);
+    }
+}
