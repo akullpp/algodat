24c65ea2587efb8ac2ba16ee9690d02df2c53dde
Andreas Kull <akullpp@gmail.com>
2019-12-06 21:28:00 +0100

    Linked List: Remove public access for better visibility

--- src/main/java/de/akull/datastructures/linkedlist/Node.java
+++ src/main/java/de/akull/datastructures/linkedlist/Node.java
@@ -1,11 +1,11 @@
 package de.akull.datastructures.linkedlist;
 
-public class Node<T> {
+class Node<T> {
 
-    public Node<T> next;
-    public T data;
+    Node<T> next;
+    T data;
 
-    public Node(T data) {
+    Node(T data) {
         this.data = data;
     }
 }
