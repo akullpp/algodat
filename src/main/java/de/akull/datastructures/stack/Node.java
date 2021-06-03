ea076fddd1ba338e1b8506bc7abf774d398cde49
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-06-03 19:05:39 +0530

    Update Node.java

--- src/main/java/de/akull/datastructures/stack/Node.java
+++ src/main/java/de/akull/datastructures/stack/Node.java
@@ -1,12 +1,13 @@
-package de.akull.datastructures.stack;
-
-
-class Node<T> {
-
-    T data;
-    Node next;
-
-    Node(T data) {
-        this.data = data;
-    }
+public class Stack {
+	private ListNode top;
+	private int length;
+	
+	private class ListNode{
+		private int data;
+		private ListNode next;
+		
+	public ListNode(int data) 
+    {
+			this.data=data;
+	}
 }
