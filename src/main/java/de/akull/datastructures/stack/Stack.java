d8cbf26ccdedcb69dc531c696f48866d8454b297
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-06-03 19:04:56 +0530

    Update Stack.java

--- src/main/java/de/akull/datastructures/stack/Stack.java
+++ src/main/java/de/akull/datastructures/stack/Stack.java
@@ -1,31 +1,69 @@
-package de.akull.datastructures.stack;
+import java.util.*;
 
-import java.util.EmptyStackException;
-
-
-class Stack {
-
-    Node head;
-
-    void push(Node n) {
-        n.next = head;
-        head = n;
-    }
-
-    Node pop() {
-        if (head == null) {
-            throw new EmptyStackException();
-        }
-        Node n = head;
-        head = head.next;
-        return n;
-    }
-
-    Node peek() {
-        return head;
-    }
-
-    boolean isEmpty() {
-        return head == null;
-    }
+// Here comes the NODE class
+	
+	public Stack()
+	{
+		top=null;
+		length=0;
+	}
+	
+	public int length()
+	{
+		return length;
+	}
+	
+	public boolean isEmpty()
+	{
+		return length==0;
+	}
+	
+	public void push()
+	{
+		System.out.println("Enter the element you want to push into the stack");
+		Scanner sc=new Scanner(System.in);
+		int value=sc.nextInt();
+		
+		ListNode temp=new ListNode(value);
+		temp.next=top;
+		top=temp;
+		length++;
+	}
+	
+	public void pop()
+	{
+		if(isEmpty())
+		{
+			System.out.println("Empty Stack!!");
+		}
+		else {
+			int result=top.data;
+			top=top.next;
+			length--;
+			System.out.println("Element popped is:" +result);
+		}
+	}
+	
+	public void peek()
+	{
+		if(isEmpty())
+		{
+			System.out.println("Empty Stack!!");
+		}
+		else {
+			System.out.println("Top most data is:" +top.data);
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		Stack st=new Stack();
+		st.push();
+		st.push();
+		st.push();
+		st.push();
+		
+		st.pop();
+		st.peek();
+	}
 }
